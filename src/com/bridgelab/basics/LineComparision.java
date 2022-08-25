91eba75726e57e81db4e380fd380a05ef1c9afb0
Nishit <nishitranjan41@gmail.com>
2022-08-25 17:23:31 +0530

    using oops concept

--- src/com/bridgelab/basics/LineComparision.java
+++ src/com/bridgelab/basics/LineComparision.java
@@ -3,36 +3,29 @@ import java.util.Scanner;
 import java.lang.*;
 public class LineComparision {
         public static void main(String[] args) {
-                System.out.println("Welcome to Line Comparison Computation Program!");
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter a Point x1");
-                int x1= sc.nextInt();
-                System.out.println("Enter a Point y1");
-                int y1= sc.nextInt();
-                System.out.println("Enter a Point x2");
-                int x2= sc.nextInt();
-                System.out.println("Enter a Point y2");
-                int y2= sc.nextInt();
-                String length1 = String.valueOf(Math.sqrt((Math.pow((x2-x1),2))+ (Math.pow((y2 -y1),2))));
-                System.out.println("length1 =>"+length1);
-                System.out.println("Enter a Point x3");
-                int x3= sc.nextInt();
-                System.out.println("Enter a Point y3");
-                int y3= sc.nextInt();
-                System.out.println("Enter a Point x4");
-                int x4= sc.nextInt();
-                System.out.println("Enter a Point y4");
-                int y4= sc.nextInt();
-                String length2 = String.valueOf(Math.sqrt((Math.pow((x4-x3),2))+(Math.pow((y4-y3),2))));
-                System.out.println("Length of 2nd Line is =>"+length2);
-                int result = length2.compareTo(length1);
-                if(result == 0){
-                        System.out.println("Both Lines are Equal !");
-                }
-                else if(result>0){
-                        System.out.println("Length of 1st Lines is Greater than 2nd Line!");
-                }else {
-                        System.out.println("Length of 2nd Line is Greater than 1st Line");
+                Points point1 = new Points();
+                Points point2 = new Points();
+                Length line2 = new Length();
+                Length line1 = new Length();
+
+                System.out.println("\nEnter points for First line\n");
+                point1.getPoints();
+                point2.getPoints();
+                Double length1 = line1.length(point1.getX(), point1.getY(), point2.getX(), point2.getY());
+                System.out.println("Length of line1 is: " + length1);
+                System.out.println("\nEnter points for second line\n");
+
+                point1.getPoints();
+                point2.getPoints();
+                Double length2 = line2.length(point1.getX(), point1.getY(), point2.getX(), point2.getY());
+                System.out.println("Length of line2 is: " + length2);
+
+                if (length1.equals(length2)) {
+                        System.out.println("\nLines are equal");
+                } else if (length1.compareTo(length2) > 0) {
+                        System.out.println("\nLine1 is greater than Line 2");
+                } else if (length1.compareTo(length2) < 0) {
+                        System.out.println("\nLine 2 is greater than Line 1");
                 }
         }
-    }
+}
